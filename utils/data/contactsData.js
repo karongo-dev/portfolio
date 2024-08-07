d5543fc4b7dc21ae2cde199b4662dedb63d973f8
karongo-dev <karongopatrick1998@gmail.com>
2024-08-07 18:45:45 +0300

    first commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'karongopatrick1998@gmail.com',
+    phone: '+254726434877',
+    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
+    github: 'https://github.com/karongo-dev',
+    // facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    // linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+    // twitter: 'https://twitter.com/said7388',
+    // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "karongo-dev"
+}
---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor Degree",
+    duration: "2018 - 2023",
+    institution: "Dedan Kimathi University of Technology",
+  },
+  {
+    id: 2,
+    title: "Kenya Certificate of Secondary Education",
+    duration: "2014 - 2017",
+    institution: "Kihara Secondary School",
+  },
+  {
+    id: 3,
+    title: "Kenya Certificate of Primary Education",
+    duration: "2005 - 2013",
+    institution: "South Muchatha Primary School",
+  }
+]
