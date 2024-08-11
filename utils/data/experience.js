77c996c3a8e2d9e963cee9cfb697dce5eaedf515
karongo-dev <karongopatrick1998@gmail.com>
2024-08-11 08:29:26 +0300

    expe

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,14 +2,14 @@ export const experiences = [
   {
     id: 1,
     title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    company: "American Center Nairobi.",
+    duration: "(Jan 2023 - Present)"
   },
   {
     id: 2,
     title: "FullStack Developer",
     company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    duration: "(Jun 2019 - Jan 2022)"
   },
   {
     id: 3,
