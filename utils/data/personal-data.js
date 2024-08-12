8f5bba2f8629a1f9f07aefa3c8357240cfe6634a
karongo-dev <karongopatrick1998@gmail.com>
2024-08-12 11:58:18 +0300

    changes

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,16 +2,16 @@ export const personalData = {
   name: "PATRICK",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "My name is Muchiri Patrick. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'karongopatrick1998@gmail.com',
   phone: '+254726434877',
-  address: 'Westlands, Nairobi, Kenya - 1212 ',
+  address: 'Westlands, Nairobi, Kenya ',
   github: 'https://github.com/karongo-dev',
   facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+  linkedIn: 'https://www.linkedin.com/in/muchiri-patrick-497622297/',
   twitter: 'https://twitter.com/said7388',
   stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
   leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  devUsername: "Karongo-Dev",
+  resume: "https://drive.google.com/file/d/1O3zcvkWNvbdMBN4hZtAYfeXKMytHbddZ/view?usp=sharing"
 }
